ff07dc31fcdfc5fb9574446a394b51e4db8df13f
Michelle Luyenga <wn1apc@gmail.com>
2019-10-04 11:50:46 +0700

    Add files via upload

---
+++ fpdf/createFont.php
@@ -0,0 +1,13 @@
+<?php
+
+require('makefont/makefont.php');
+
+MakeFont('C:/xampp/htdocs/db2proj/fpdf/font/THSarabunNew.ttf','cp874');
+
+MakeFont('C:/xampp/htdocs/db2proj/fpdf/font/THSarabunNew Bold.ttf','cp874');
+
+MakeFont('C:/xampp/htdocs/db2proj/fpdf/font/THSarabunNew Italic.ttf','cp874');
+
+MakeFont('C:/xampp/htdocs/db2proj/fpdf/font/THSarabunNew BoldItalic.ttf','cp874');
+
+?>
